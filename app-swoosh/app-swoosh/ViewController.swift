00427452d31c8f81203a04905ac8dc52a8965b69
Kieran <kieranoliver@Kierans-MBP.home>
2018-01-24 14:48:01 +0000

    minor changes

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Kieran on 22/01/2018.
+//  Copyright © 2018 Kieran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgimg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
